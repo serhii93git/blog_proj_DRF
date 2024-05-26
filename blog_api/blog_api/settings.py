826c907941bfb791e88c07ccb2929fa585bc2835
serhii <1@K.RR>
2024-05-26 15:11:12 +0200

    add corsheaders

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -44,11 +44,15 @@ INSTALLED_APPS = [
     'post.apps.PostConfig',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]

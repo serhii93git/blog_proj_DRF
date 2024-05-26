966ca0698bcb87d5a268bd1d18c3ccc2d31f28ee
serhii <1@K.RR>
2024-05-26 14:54:39 +0200

    register Post model in admin panel

--- blog_api/post/admin.py
+++ blog_api/post/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+#  admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = '__all__'
+    search_fields = ('title', 'text')
+    list_filter = ('author',)
+    readonly_fields = ('created_at', 'updated_at')

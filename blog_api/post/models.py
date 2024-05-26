797c151aa578b2eb3214b8eeaf73b1428619fbcb
serhii <1@K.RR>
2024-05-26 14:46:52 +0200

    craeted Post model

--- blog_api/post/models.py
+++ blog_api/post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Model for blog posts"""
+
+    title = models.CharField(max_length=150)
+    text = models.TextField(blank=True, null=True)
+    media = models.FileField(upload_to='files/', blank=True, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+
+    author = models.CharField(max_length=50)  # to do: connect to User model
+
+    def __str__(self):
+        return self.title

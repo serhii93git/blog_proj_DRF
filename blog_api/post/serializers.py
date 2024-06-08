577d3f28657f667e98bf120ff5e918c87d47051f
serhii93git <s.g.demchenko93@gmail.com>
2024-06-08 19:42:20 +0200

    set author in post serializer ae read only

--- blog_api/post/serializers.py
+++ blog_api/post/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import Post
-from creator.models import Creator
 
 
 class PostSerializer(serializers.ModelSerializer):
     """Serializer for Post model"""
     # text = serializers.SerializerMethodField()
     author = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
         model = Post
         fields = '__all__'
